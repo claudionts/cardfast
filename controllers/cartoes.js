cfcfee9669617b4b0e85696da5b8cb04b0728340
claudiomeditool <claudionts@gmail.com>
2018-04-26 23:31:14 -0300

    Alteração Controllers

--- controllers/cartoes.js
+++ controllers/cartoes.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
       console.log('processando pagamento com cartão');
   
         var cartao = req.body;
-  
+      
         req.assert("numero", "Número é obrigatório e deve ter 16 caracteres.").notEmpty().len(16,16);
         req.assert("bandeira", "Bandeira do cartão é obrigatória.").notEmpty();
         req.assert("ano_de_expiracao", "Ano de expiração é obrigatório e deve ter 4 caracteres.").notEmpty().len(4,4);
